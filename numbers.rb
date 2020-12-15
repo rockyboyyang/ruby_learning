dc08bb8ccd1d35e01867794b6f41328deff1ed6f
Rocky <yangrocky2013@gmail.com>
2020-12-14 20:41:06 -0600

    learn how to do math operations in ruby

---
+++ numbers.rb
@@ -0,0 +1,10 @@
+# puts 1 + 1
+# puts -5 + 3
+# puts 2.5 + 0.5
+# puts 2 * 2
+# puts 19 % 3
+# puts 5 / 2 # Prints out 2 but really the answer should be 2.5
+# puts 5 / 2.0 # Should have one of the nums as a decimal to get a decimal output
+
+puts 6 + 4 / 2 # 8 
+puts (6 + 4) / 2 # 5
