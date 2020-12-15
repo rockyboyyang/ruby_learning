c66a2ff9247906769590969610aff29a96c7bf3b
Rocky <yangrocky2013@gmail.com>
2020-12-14 21:05:56 -0600

    learn boolean syntax

---
+++ boolean.rb
@@ -0,0 +1,11 @@
+# Boolean Data Type Lecture
+
+# Operations:
+# ! (not)
+# && (and)
+# || (or)
+
+puts false && true # false
+puts false && true # false
+puts true && false # false
+puts true && true  # true
