fe376292930e1d053694a052aa272806b55ad9d8
Rocky <yangrocky2013@gmail.com>
2020-12-14 20:46:17 -0600

    practiced comparisons in ruby

--- numbers.rb
+++ numbers.rb
@@ -6,5 +6,24 @@
 # puts 5 / 2 # Prints out 2 but really the answer should be 2.5
 # puts 5 / 2.0 # Should have one of the nums as a decimal to get a decimal output
 
-puts 6 + 4 / 2 # 8 
-puts (6 + 4) / 2 # 5
+# puts 6 + 4 / 2 # 8 
+# puts (6 + 4) / 2 # 5
+
+
+# Ruby Number Comparisons
+
+# equal ==
+# not equal !=
+# greater than >
+# less than <
+# greater than or equal to >=
+# less than or equal to <=
+
+puts 5 == 5 # true
+puts 3 == 5 # false
+puts 4 != 6 # true
+puts 2 != 2 # false
+puts 7 > 4 # true
+puts 3 > 5 # false
+puts 1 < 9 # true
+puts 4 < 2 # false
