089ff88ca2f9c5c6cf3065daa9c56565a2ffeaf4
Rocky <yangrocky2013@gmail.com>
2020-12-15 18:48:40 -0600

    learned methods

---
+++ methods.rb
@@ -0,0 +1,20 @@
+# Methods
+
+# Defining a method
+def sayMessage
+    puts "Hey Programmers!"
+    puts "What's for lunch?"
+end
+
+# Call this method
+# puts "before the method call"
+# sayMessage
+# puts "after the method call"
+
+def isEven(num)
+    return num % 2 == 0
+end
+
+puts isEven(41)
+
+
