0a22cf1e06b0613816d6e40c2f8ae182085f5c0a
Rocky <yangrocky2013@gmail.com>
2020-12-16 21:07:06 -0600

    worked on arrays in ruby

---
+++ arrays.rb
@@ -0,0 +1,21 @@
+# Arrays
+
+names = ["John", "Dave", "Pau"]
+# p names
+
+# adds new element to the end of the array
+names << "Marc"
+# p names
+
+names[1] = "Gasol"
+# p names
+
+
+foods = ['pizza', 'sushi', 'pho', 'fries', 'burgers']
+
+i = 0
+while i < foods.length
+    p foods[i]
+    i += 1
+end
+
